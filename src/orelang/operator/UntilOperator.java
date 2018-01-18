1e8c02bcc5fe5c7dc8cd84aca2b438acb5239024
masayuki tanaka <masayuki5160@gmail.com>
2018-01-18 23:49:29 +0900

    コンパイルエラー対応

--- src/orelang/operator/UntilOperator.java
+++ src/orelang/operator/UntilOperator.java
@@ -9,7 +9,7 @@ public class UntilOperator implements IOperator {
 	@Override
 	public Object call(Engine engine, List<?> args) {
 		Object retVal = null;
-		while(!(boolean)engine.eval(args.get(0))) {
+		while(!(Boolean)engine.eval(args.get(0))) {
 			retVal = engine.eval(args.get(1));
 		}
 		return retVal;
