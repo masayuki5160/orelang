693fd291240af4ce40d640b5e2616ec99a5e35e8
masayuki tanaka <masayuki5160@gmail.com>
2018-01-18 22:10:46 +0900

    事前処理を追加

--- src/orelang/TestOrelang.java
+++ src/orelang/TestOrelang.java
@@ -2,16 +2,22 @@ package orelang;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
-
 import junit.framework.Assert;
 import net.arnx.jsonic.JSON;
 
 public class TestOrelang {
+	
+	Engine engine;
+
+	@Before
+	public void setUp() {
+		engine = new Engine();
+	}
 
 	@Test
 	public void 簡単な四則演算を実行できる() {
-		Engine engine = new Engine();
 		Object result = engine.eval(JSON.decode("[\"+\", 1, 2, [\"*\", 3, 4]]"));
 		
 		// TODO toString()してるけど他に方法がないか？
