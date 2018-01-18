222a48ef7442e8c10190fac6e68c3eaa5a702b50
masayuki tanaka <masayuki5160@gmail.com>
2018-01-18 23:30:24 +0900

    テストケース追加

--- src/orelang/TestOrelang.java
+++ src/orelang/TestOrelang.java
@@ -10,6 +10,18 @@ import net.arnx.jsonic.JSON;
 public class TestOrelang {
 	
 	Engine engine;
+	// TODO 外部ファイルに出したい
+	String example = 
+			"[\"step\",\n" + 
+			"  [\"set\", \"i\", 10],\n" + 
+			"  [\"set\", \"sum\", 0],\n" + 
+			"  [\"until\", [\"=\", [\"get\", \"i\"], 0], [\n" + 
+			"    \"step\",\n" + 
+			"    [\"set\", \"sum\", [\"+\", [\"get\", \"sum\"], [\"get\", \"i\"]]],\n" + 
+			"    [\"set\", \"i\", [\"+\", [\"get\", \"i\"], -1]]\n" + 
+			"  ]],\n" + 
+			"  [\"get\", \"sum\"]\n" + 
+			"]";
 
 	@Before
 	public void setUp() {
@@ -22,6 +34,13 @@ public class TestOrelang {
 		
 		// TODO toString()してるけど他に方法がないか？
 		assertThat(result.toString(), is("15"));
+	}
+	
+	@Test
+	public void stepを利用したコードを実行できる() {
+		Object result = engine.eval(JSON.decode(example));
+		// TODO toString()してるけど他に方法がないか？
+		assertThat(result.toString(), is("55"));
 	}
 
 }
