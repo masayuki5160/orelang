9fd0f3290f45a956577684019fbe49b31d9eb49c
masayuki tanaka <masayuki5160@gmail.com>
2018-01-19 22:39:58 +0900

    Stepオペレータのテストプログラムを外部ファイルにだす

--- src/orelang/test/TestOrelang.java
+++ src/orelang/test/TestOrelang.java
@@ -17,18 +17,6 @@ import orelang.Engine;
 public class TestOrelang {
 	
 	Engine engine;
-	// TODO 外部ファイルに出したい
-	String example = 
-			"[\"step\",\n" + 
-			"  [\"set\", \"i\", 10],\n" + 
-			"  [\"set\", \"sum\", 0],\n" + 
-			"  [\"until\", [\"=\", [\"get\", \"i\"], 0], [\n" + 
-			"    \"step\",\n" + 
-			"    [\"set\", \"sum\", [\"+\", [\"get\", \"sum\"], [\"get\", \"i\"]]],\n" + 
-			"    [\"set\", \"i\", [\"+\", [\"get\", \"i\"], -1]]\n" + 
-			"  ]],\n" + 
-			"  [\"get\", \"sum\"]\n" + 
-			"]";
 
 	@Before
 	public void setUp() {
@@ -44,8 +32,8 @@ public class TestOrelang {
 	}
 	
 	@Test
-	public void stepを利用したコードを実行できる() {
-		Object result = engine.eval(JSON.decode(example));
+	public void stepを利用したコードを実行できる() throws JSONException, FileNotFoundException, IOException {
+		Object result = engine.eval(JSON.decode(new FileReader("src/orelang/test/testStepOp.json")));
 		// TODO toString()してるけど他に方法がないか？
 		assertThat(result.toString(), is("55"));
 	}
